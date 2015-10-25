f00397ccbe01ef2fb07c851171d5417f10740cdf
Svale Fossåskaret <svale@3by5.no>
2015-10-25 23:05:52 +0100

    Added routing

---
+++ public_html/scripts/router.js
@@ -0,0 +1,21 @@
+/*global window, Router, storyTeller */
+(function (window, Router, storyTeller) {
+    'use strict';
+
+    // We're using https://github.com/flatiron/director for routing
+    var router = new Router({
+        '/page/:slideId': function (slideId) {
+            storyTeller.set('current', +slideId); // cast string-parameter to number
+        }
+    });
+
+    router.configure({
+        notfound: function () {
+            window.location.hash = '';
+            storyTeller.set('current', 1);
+        }
+    });
+
+    router.init();
+
+})(window, Router, storyTeller);
